27ac8594399070176bb802247352ad5d0d984db6
Jordan Perry <jperry@Jordans-MacBook-Pro.local>
2022-01-27 18:29:56 -0600

    kick off container build

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
+  console.log('kick off container build');
   return (
     <div>
       <h1>Hi there!</h1>
