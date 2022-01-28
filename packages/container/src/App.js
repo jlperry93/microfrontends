3b21fa34145ea502f118fcb9bb3a471d2253acd2
Jordan Perry <jperry@Jordans-MacBook-Pro.local>
2022-01-28 11:51:31 -0600

    added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,17 @@
 
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 export default () => {
   console.log('trigger container build');
   return (
-    <div>
-      <h1>Hi there!!!!!</h1>
-      <hr />
-      <MarketingApp ></MarketingApp>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp ></MarketingApp>
+      </div>
+    </BrowserRouter>
   )
 };
