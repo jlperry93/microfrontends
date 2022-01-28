a113e45694dc76416a0715b8c7a262bdf3edcfc8
Jordan Perry <jperry@Jordans-MacBook-Pro.local>
2022-01-28 12:26:48 -0600

    add custom class names for styles

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,15 +3,22 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+});
 
 export default () => {
   console.log('trigger container build');
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp ></MarketingApp>
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp ></MarketingApp>
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   )
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma'
+});
+
 export default () => {
   return (
   <div>
-    <StylesProvider>
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/pricing" component={Pricing} />
-          <Route path="/" component={Landing} />
-        </Switch>
-      </BrowserRouter>
-    </StylesProvider>
+        <StylesProvider generateClassName={generateClassName}>
+          <Switch>
+            <Route exact path="/pricing" component={Pricing} />
+            <Route path="/" component={Landing} />
+          </Switch>
+        </StylesProvider>
+      </BrowserRouter> 
   </div>
   )
 }
