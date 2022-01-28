917ffdd1f944749cdd1ddbaea55252748f62ac1d
Jordan Perry <jperry@Jordans-MacBook-Pro.local>
2022-01-28 10:58:18 -0600

    trigger build

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-  console.log('kick off container build');
+  console.log('trigger container build');
   return (
     <div>
       <h1>Hi there!</h1>
